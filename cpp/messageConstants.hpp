1d176e18a2d0bcef2b9ff5d36cdea255980406b9
Johannes Kern <johannes.kern@zoho.com>
2020-04-20 21:38:05 +0200

    Test hooks

---
+++ cpp/messageConstants.hpp
@@ -0,0 +1,22 @@
+#pragma once
+
+#define ID_OFSSET 0
+#define CMD_OFFSET 4
+#define KEY_OFFSET 8
+#define SIZE_OFFSET 14
+#define PAYLOAD_OFFSET 16
+
+namespace dh {
+
+enum MessageCommand { 
+    READ: 1,
+    WRITE: 2,
+    PRINT: 3
+};
+enum MessageKey { 
+    P_ACTBTS: 1,
+    P_SDRLEN: 2,
+    S_INPUT: 1001,
+    S_SPOOL: 1002
+};
+}
