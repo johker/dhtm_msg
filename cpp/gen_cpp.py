9b1bbfa419798806d5d1d9e245c43b46efb15ba1
Johannes Kern <johannes.kern@zoho.com>
2020-04-20 14:32:17 +0200

    Code generation for defines

--- cpp/gen_cpp.py
+++ cpp/gen_cpp.py
@@ -11,10 +11,17 @@ with open(os.path.dirname(__file__) + "/../msg_ids.yaml", 'r') as stream:
 
 with open(os.path.dirname(__file__) + '/messageConstants.hpp', 'w') as fout:
     fout.write('#pragma once\n\n')
-    fout.write('namespace dh {\n\n')
     for x,y in data.items():
         # TODO: #define for len(y) == 1
-        if(len(y)>1):
+        if type(y) is int:
+            fout.write('#define ' + x + ' ' + str(y) + '\n')
+
+    fout.write('\n')
+    fout.write('namespace dh {\n\n')
+
+    for x,y in data.items():
+        print(type(y))
+        if type(y) is list:
             fout.write('enum ' + x + ' { \n')
             for yi in y[:-1]:
                 k = list(yi)[0];
